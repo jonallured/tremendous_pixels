b26bbd8ee6bd3b480515fa238b3b28c56e2ea787
Jon Allured <jon.allured@gmail.com>
2017-03-20 10:21:46 -0500

    Sort by created_at

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,5 @@
 class ImagesController < ApplicationController
-  expose(:images) { Image.all }
+  expose(:images) { Image.order(:created_at) }
   def show
     image = Image.find_by id: params[:id]
     render body: image.data, content_type: 'image/png'
