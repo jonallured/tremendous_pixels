48198569e34028779b1e34b05867dbf9b72343fa
Jon Allured <jon.allured@gmail.com>
2017-03-20 10:13:00 -0500

    Add gallery page

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,5 @@
 class ImagesController < ApplicationController
+  expose(:images) { Image.all }
   def show
     image = Image.find_by id: params[:id]
     render body: image.data, content_type: 'image/png'
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   mount ActionCable.server => :cable
   get :live, to: 'live#show'
   get 'images/:id', to: 'images#show', format: 'png'
+  get 'images', to: 'images#index'
 end
