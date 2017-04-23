d6303cf1ad6b744581c7df1c582f0d6cceb43156
Jon Allured <jon.allured@gmail.com>
2017-04-23 07:16:51 -0500

    Restore iPad styles

--- app/controllers/live_controller.rb
+++ app/controllers/live_controller.rb
@@ -1,3 +1,8 @@
 class LiveController < ApplicationController
   expose(:image) { Image.most_recent }
+
+  def show
+    layout = params[:layout] || 'application'
+    render :show, layout: layout
+  end
 end
