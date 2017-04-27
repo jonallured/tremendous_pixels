19808af56375e93abd7c7dfb95cd8fca1fc7b30d
Jon Allured <jon.allured@gmail.com>
2017-04-27 10:26:21 -0500

    Tweet images as they are made

--- app/jobs/transform_tweet_job.rb
+++ app/jobs/transform_tweet_job.rb
@@ -2,7 +2,20 @@ class TransformTweetJob < ActiveJob::Base
   def perform(target_tweet_id)
     target_tweet = TargetTweet.find_by id: target_tweet_id
     return unless target_tweet
-    TweetTransformer.transform(target_tweet)
-    ActionCable.server.broadcast 'live', image_src: Image.most_recent.to_src
+    image = TweetTransformer.transform(target_tweet)
+    tweet_image(image)
+    ActionCable.server.broadcast 'live', image_src: image.to_src
+  end
+
+  private
+
+  def tweet_image(image)
+    path = File.join Rails.root, 'tmp', image.to_src
+    FileUtils.mkdir_p File.dirname path
+    File.binwrite path, image.data
+    file = File.new path
+    client = TwitterClient.generate
+    text = "http://tremendouspixels.com/images/#{image.id}"
+    client.update_with_media text, file
   end
 end
